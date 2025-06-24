c1edd749c705d354d691ae7df109b96579d17a28
RedTorres <155429228+florantetorres@users.noreply.github.com>
2025-06-24 21:25:02 +0800

    Second

---
+++ src/task2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Florante Torres
+ */
+public class task2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char h ='H';
+        short shorty = 311;
+        byte zero = 0;
+        char W = 'w';
+        char R = 'r';
+        byte one = 1;
+        char d = 'd';
+        String output = "" + h + shorty + zero + " " +W + zero + R + one + d;
+        System.out.println(output);
+        }
+
+    }
